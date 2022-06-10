8e28016126a26c8576605951829d01cf7617d64b
Leandro Felix <lehinfo.felix@gmail.com>
2022-06-10 16:33:37 -0300

    Criação com POO

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 // * Importação dos módulos <fs> para manipulação de arquivos
 const fs = require('node:fs');
 
+// FUNCOES SORT
+function menorChegada(a, b) {
+    if (a.chegada < b.chegada) {
+        return -1;
+    }
+    if (a.chegada > b.chegada) {
+        return 1;
+    }
+    return 0;
+}
+
+function menorRajada(a, b) {
+    if (a.rajada < b.rajada) {
+        return -1;
+    }
+    if (a.rajada > b.rajada) {
+        return 1;
+    }
+    return 0;
+}
+
 // * Definição das flags de execução do sistema:
 // < urlCSV > [Armazena o endereço do arquivo CSV]
 // < modo > [Refere-se ao modo de escalonamento dos processos]
@@ -12,41 +33,62 @@ const modoExecucao = Number(process.argv[4]);
 
 const arquivoCSV = fs.readFileSync(urlCSV, 'utf-8');
 
-// Declaração das variáveis auxiliares
-let processo, idProcesso, chegadaProcesso, rajadaProcesso, prioridadeProcesso, tempoTotalProcessamento, tempoTotalEspera, quantidadeTrocasContexto, tempoFinalizacao, inicioProcesso;
+// Armazena todas as instancias de processos
+var processos = new Array();
 
-let tempo = 0;
-let tempoRestante = 0;
-let mediaTempoDeRetorno = 0;
-let mediaTempoEspera = 0;
-// Vetor que armazena os IDS na ordem de finalização dos processos
-let ordemfinalizacao = new Array;
-// Vetor que armazena os o tempo de retono de cada processo
-let tempoDeRetorno = new Array;
-// Vetor que armazena os tempos de finalização de cada processo
-let finalizacaoProcesso = new Array;
-// Vetor que armazena os tempos de espera de cada processo
-let tempoEspera = new Array;
+// Armazena o tempo
+var tempo = 1;
+
+class Processo {
+
+    constructor(id, chegada, rajada, prioridade) {
+        this.id = id;
+        this.chegada = chegada;
+        this.rajada = rajada;
+        this.prioridade = prioridade;
+        this.comeco = undefined;
+        this.termino = undefined;
+        this.contexto = undefined;
+        this.terminado = false;
+    }
+
+    debug() {
+        console.log("id -> ", this.id, "chegada ->", this.chegada, "rajada -> ", this.rajada, "prioridade ->", this.prioridade);
+    }
+}
 
 console.log({ urlCSV, modoEscalonamento, modoExecucao });
 
 // Leitura do arquivo CSV através da biblioteca fs:node
-const processos = arquivoCSV.split('\n');
+const arquivo = arquivoCSV.split('\n');
+
+// Para  cada linha do arquivo csv
+// exceto a primeira, pois são os cabeçalhos
+for (let linha = 1; linha < arquivo.length; linha++) {
+    // PID | chegada | rajada | prioridade
+    let split = arquivo[linha].split(",");
+
+    // cria um processo com os dados acima
+    let processo = new Processo(split[0][1], split[1], split[2], split[3][0]);
+
+    // envia o processo para a variavel global processos
+    processos.push(processo);
+}
 
 // Determina a função a ser executada baseada no modo de escalonamento
 switch (modoEscalonamento) {
     case "TEST":
         //Execução da função de teste, para debug da aplicação
-        test(idProcesso, chegadaProcesso, rajadaProcesso, prioridadeProcesso);
+        test();
         break;
     case "FCFS":
-        fcfs(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera);
+        fcfs(modoExecucao);
         break;
     case "SRTF":
-        srtf(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera, tempoRestante);
+        srtf();
         break;
     case "RR":
-        rr(modoExecucao);
+        rr();
         break;
     case "SRTPF":
         srtpf(modoExecucao);
@@ -64,129 +106,133 @@ switch (modoEscalonamento) {
 }
 
 /* MODO DE DEBUG PARA O DESENVOLVIMENTO */
-function test(idProcesso, chegadaProcesso, rajadaProcesso, prioridadeProcesso) {
+function test() {
+    console.log("oi");
+}
 
-    for (let i = 1; i < processos.length; i++) {
-        // Separação do objeto processos em um array para cada processo
-        processo = processos[i].split(',');
+function fcfs(modoExecucao) {
+    // filtra todos os processos na ordem de chegada
+    let ordemChegada = processos.sort(menorChegada);
 
-        // Atribuição dos elementos de cada array() em uma variável distinta
-        idProcesso = processo[0];
-        chegadaProcesso = Number(processo[1]);
-        rajadaProcesso = Number(processo[2]);
-        prioridadeProcesso = Number(processo[3]);
+    // enquanto o ultimo processo nao for terminado
+    while (!ordemChegada[ordemChegada.length - 1].terminado) {
+        // checa se o primeiro processo terminou
+        if (ordemChegada[0].terminado) {
+            // se tiver terminado, tira o processo da fila
+            ordemChegada.shift();
+        }
 
-        console.log("# Processo", i);
-        console.log("ID:", idProcesso);
-        console.log("Chegada:", chegadaProcesso);
-        console.log("Rajada:", rajadaProcesso)
-        console.log("Prioridade:", prioridadeProcesso);
-        console.log("\n ====================================================== \n");
+        // se nao tiver terminado, executa outra vez
+        ordemChegada[0].rajada--;
+
+        // checa se foi a ultima execucao
+        if (ordemChegada[0].rajada == 0) {
+            // se for, anota que terminou
+            ordemChegada[0].terminado = true;
+            ordemChegada[0].termino = tempo;
+        }
+
+        // aumenta o tempo
+        tempo++;
     }
 }
 
-/* MODO DE EXECUÇÃO FCFS */
-function fcfs(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera) {
 
-    for (let i = 1; i < processos.length; i++) {
-        // Separação do objeto processos em um array para cada processo
-        processo = processos[i].split(',');
+function srtfp(modoExecucao) {
+    // armazena quem ja chegou
+    let chegados = new Array;
 
-        // Atribuição dos elementos de cada array() em uma variável distinta
-        idProcesso = processo[0];
-        chegadaProcesso = Number(processo[1]);
-        rajadaProcesso = Number(processo[2]);
-        prioridadeProcesso = Number(processo[3]);
-        inicioProcesso = tempo;
-
-        ordemfinalizacao.push(idProcesso);
-
-        tempoEspera.push(inicioProcesso - chegadaProcesso + 1);
-
-        do {
-            rajadaProcesso--;
-            tempo++;
-        } while (rajadaProcesso > 0);
-
-        tempoDeRetorno.push(tempo - chegadaProcesso);
-    }
-
-    for (let i = 0; i < tempoDeRetorno.length; i++) {
-        mediaTempoDeRetorno += tempoDeRetorno[i];
-    }
-    for (let i = 0; i < tempoEspera.length; i++) {
-        mediaTempoEspera += tempoEspera[i];
-    }
-
-    mediaTempoDeRetorno /= tempoDeRetorno.length;
-    mediaTempoEspera /= tempoEspera.length;
-
-
-    //* O vetor processo já está na ordem de finalização correta, logo, não necessita ser alterado.
-    switch (modoExecucao) {
-        case 1:
-            // FCFS == MODO 1 ==
-            for (let i = 0; i < processos.length - 1; i++) {
-                console.log("O processo", ordemfinalizacao[i], "encerrou no tempo: ", finalizacaoProcesso[i]);
-
-            }
-            break;
-        case 2:
-            // FCFS == MODO 2 ==
-            //Tempo total de execução
-            console.log("\n ======================================= \n");
-            console.log("* O tempo total de execução do processamento foi de", tempo, "unidades de tempo\n");
-
-            //Media de tempo de retorno
-            console.log("* A média de tempo de retorno é de:", mediaTempoDeRetorno, "unidedes de tempo\n");
-
-            //Media de tempo de espera
-            console.log("* A média de espera dos processos é de: ", mediaTempoEspera, "unidades de tempo");
-            //Trocas de contexto
-            break;
-        default:
-            console.log("Valor inválido");
-            break;
-    }
-
-}
-function srtf(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera, tempoRestante) {
-    // Separação do objeto processos em um array para cada processo
-
-    let processosSrtf = new Array;
-    let processosChegados = new Array;
-    let procesosFinalizados = new Array;
-
-
-    do {
-        // 1-  Armazenar todos os processos que chegaram ate o momento
-        for (let i = 1; i < processos.length; i++) {
-            processo = processos[i].split(',');
-
-            // Atribuição dos elementos de cada array() em uma variável distinta
-            idProcesso = processo[0];
-            chegadaProcesso = Number(processo[1]);
-            rajadaProcesso = Number(processo[2]);
-            prioridadeProcesso = Number(processo[3]);
-            inicioProcesso = tempo;
-
-            if (tempo >= chegadaProcesso) {
-                // checa se o processo ja esta no array
-                // caso não esteja, adiciona
-                if (!processosSrtf.includes(idProcesso)) {
-                    processosSrtf.push(idProcesso);
-                }
+    // enquanto o ultimo processo nao for terminado
+    while (!processos[processos.length - 1].terminado) {
+        // adiciona os processos que ja chegaram ao array chegados
+        for (processo of processos) {
+            if (tempo == processo.chegada) {
+                chegados.push(processo);
             }
         }
-        console.log(tempo, "====", processosSrtf);
 
+        // filtra o array dos chegados
+        let ordemRajada = chegados.sort(menorRajada)
 
-        // 2- checa se o processo foi completado e atualiza os dados caso tenha sido
-        // 3- popula o vetor dos chegados com processos que nao tenham sido terminados
-        // 4- filtra o vetor temporario e identifica o processo que devera ser executado no instante queueTime
+        ordemRajada[0].debug();
+        // checa se o primeiro processo terminou
+        if (ordemRajada[0].terminado) {
+            // se tiver terminado, tira o processo da fila
+            ordemRajada.shift();
+        }
 
+        // se nao tiver terminado, executa outra vez
+        ordemRajada[0].rajada--;
+
+        // checa se foi a ultima execucao
+        if (ordemRajada[0].rajada == 0) {
+            // se for, anota que terminou
+            ordemRajada[0].terminado = true;
+            ordemRajada[0].termino = tempo;
+        }
+
+        // aumenta o tempo
         tempo++;
-    } while (procesosFinalizados.length != processos.length - 1);
+    }
+
+    console.log(processos[processos.length -1].termino);
+
 }
 
+function srtf(modoExecucao) {
+    // armazena quem ja chegou
+    let chegados = new Array;
+    // armazena na ordem de menor rajada
+    let ordemRajada = new Array;
+
+    // calcula o tempo maximo de execucao
+    let tempoMaximo = 0;
+
+    for(processo of processos){
+        tempoMaximo+=parseInt(processo.rajada);
+    }
+
+    console.log(tempoMaximo)
+    // enquanto o ultimo processo nao for terminado
+    while (tempo <= tempoMaximo) {
+
+        // adiciona os processos que ja chegaram ao array chegados
+        for (processo of processos) {
+            if (tempo == processo.chegada) {
+                chegados.push(processo);
+            }
+        }
+
+        // filta a primeira vez
+        if (tempo == 1){
+            ordemRajada = chegados.sort(menorRajada);
+        }
+
+        console.log(tempo);
+        ordemRajada[0].debug();
+        // checa se o primeiro processo terminou
+        if (ordemRajada[0].terminado) {
+            // se tiver terminado, tira o processo da fila
+            ordemRajada.shift();
+
+            // filtra o array dos chegados
+            ordemRajada = chegados.sort(menorRajada)
+        }
+
+        // se nao tiver terminado, executa outra vez
+        ordemRajada[0].rajada--;
+
+        // checa se foi a ultima execucao
+        if (ordemRajada[0].rajada == 0) {
+            // se for, anota que terminou
+            ordemRajada[0].terminado = true;
+            ordemRajada[0].termino = tempo;
+        }
+
+        // aumenta o tempo
+        tempo++;
+    }
+    // console.log(processos[processos.length -1].termino);
+
+}
 /* ======================================================= */
---
+++ src/indexArray.js
@@ -0,0 +1,201 @@
+// * Importação dos módulos <fs> para manipulação de arquivos
+const fs = require('node:fs');
+
+// * Definição das flags de execução do sistema:
+// < urlCSV > [Armazena o endereço do arquivo CSV]
+// < modo > [Refere-se ao modo de escalonamento dos processos]
+// < modoExecucao > [Refere-se a exibição simplificada ou detalhada]
+
+const urlCSV = process.argv[2];
+const modoEscalonamento = process.argv[3];
+const modoExecucao = Number(process.argv[4]);
+
+const arquivoCSV = fs.readFileSync(urlCSV, 'utf-8');
+
+// Declaração das variáveis auxiliares
+let processo, idProcesso, chegadaProcesso, rajadaProcesso, prioridadeProcesso, tempoTotalProcessamento, tempoTotalEspera, quantidadeTrocasContexto, tempoFinalizacao, inicioProcesso;
+
+let tempo = 0;
+let tempoRestante = 0;
+let mediaTempoDeRetorno = 0;
+let mediaTempoEspera = 0;
+// Vetor que armazena os IDS na ordem de finalização dos processos
+let ordemfinalizacao = new Array;
+// Vetor que armazena os o tempo de retono de cada processo
+let tempoDeRetorno = new Array;
+// Vetor que armazena os tempos de finalização de cada processo
+let finalizacaoProcesso = new Array;
+// Vetor que armazena os tempos de espera de cada processo
+let tempoEspera = new Array;
+
+console.log({ urlCSV, modoEscalonamento, modoExecucao });
+
+// Leitura do arquivo CSV através da biblioteca fs:node
+const processos = arquivoCSV.split('\n');
+
+// Determina a função a ser executada baseada no modo de escalonamento
+switch (modoEscalonamento) {
+    case "TEST":
+        //Execução da função de teste, para debug da aplicação
+        test(idProcesso, chegadaProcesso, rajadaProcesso, prioridadeProcesso);
+        break;
+    case "FCFS":
+        fcfs(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera);
+        break;
+    case "SRTF":
+        srtf(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera, tempoRestante);
+        break;
+    case "RR":
+        rr(modoExecucao);
+        break;
+    case "SRTPF":
+        srtpf(modoExecucao);
+        break;
+    case "PRIO":
+        prio(modoExecucao);
+        break;
+    case "PRIOP":
+        priop(modoExecucao);
+        break;
+
+    default:
+        console.log("Insira um parâmetro válido");
+        break;
+}
+
+/* MODO DE DEBUG PARA O DESENVOLVIMENTO */
+function test(idProcesso, chegadaProcesso, rajadaProcesso, prioridadeProcesso) {
+
+    for (let i = 1; i < processos.length; i++) {
+        // Separação do objeto processos em um array para cada processo
+        processo = processos[i].split(',');
+
+        // Atribuição dos elementos de cada array() em uma variável distinta
+        idProcesso = processo[0];
+        chegadaProcesso = Number(processo[1]);
+        rajadaProcesso = Number(processo[2]);
+        prioridadeProcesso = Number(processo[3]);
+
+        console.log("# Processo", i);
+        console.log("ID:", idProcesso);
+        console.log("Chegada:", chegadaProcesso);
+        console.log("Rajada:", rajadaProcesso)
+        console.log("Prioridade:", prioridadeProcesso);
+        console.log("\n ====================================================== \n");
+    }
+}
+
+/* MODO DE EXECUÇÃO FCFS */
+function fcfs(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera) {
+
+    for (let i = 1; i < processos.length; i++) {
+        // Separação do objeto processos em um array para cada processo
+        processo = processos[i].split(',');
+
+        // Atribuição dos elementos de cada array() em uma variável distinta
+        idProcesso = processo[0];
+        chegadaProcesso = Number(processo[1]);
+        rajadaProcesso = Number(processo[2]);
+        prioridadeProcesso = Number(processo[3]);
+        inicioProcesso = tempo;
+
+        ordemfinalizacao.push(idProcesso);
+
+        tempoEspera.push(inicioProcesso - chegadaProcesso + 1);
+
+        do {
+            rajadaProcesso--;
+            tempo++;
+        } while (rajadaProcesso > 0);
+
+        tempoDeRetorno.push(tempo - chegadaProcesso);
+    }
+
+    for (let i = 0; i < tempoDeRetorno.length; i++) {
+        mediaTempoDeRetorno += tempoDeRetorno[i];
+    }
+    for (let i = 0; i < tempoEspera.length; i++) {
+        mediaTempoEspera += tempoEspera[i];
+    }
+
+    mediaTempoDeRetorno /= tempoDeRetorno.length;
+    mediaTempoEspera /= tempoEspera.length;
+
+
+    //* O vetor processo já está na ordem de finalização correta, logo, não necessita ser alterado.
+    switch (modoExecucao) {
+        case 1:
+            // FCFS == MODO 1 ==
+            for (let i = 0; i < processos.length - 1; i++) {
+                console.log("O processo", ordemfinalizacao[i], "encerrou no tempo: ", finalizacaoProcesso[i]);
+
+            }
+            break;
+        case 2:
+            // FCFS == MODO 2 ==
+            //Tempo total de execução
+            console.log("\n ======================================= \n");
+            console.log("* O tempo total de execução do processamento foi de", tempo, "unidades de tempo\n");
+
+            //Media de tempo de retorno
+            console.log("* A média de tempo de retorno é de:", mediaTempoDeRetorno, "unidedes de tempo\n");
+
+            //Media de tempo de espera
+            console.log("* A média de espera dos processos é de: ", mediaTempoEspera, "unidades de tempo");
+            //Trocas de contexto
+            break;
+        default:
+            console.log("Valor inválido");
+            break;
+    }
+
+}
+function srtf(modoExecucao, ordemfinalizacao, finalizacaoProcesso, idProcesso, tempoTotalProcessamento, tempo, tempoDeRetorno, mediaTempoDeRetorno, inicioProcesso, tempoEspera, mediaTempoEspera, tempoRestante) {
+    // Separação do objeto processos em um array para cada processo
+
+    let processosSrtf = new Array;
+    let processosChegados = new Array;
+    let procesosFinalizados = new Array;
+    let processosSrtfRajada = new Array;
+
+
+
+    do {
+        // 1-  Armazenar todos os processos que chegaram ate o momento
+        for (let i = 1; i < processos.length; i++) {
+            processo = processos[i].split(',');
+
+            // Atribuição dos elementos de cada array() em uma variável distinta
+            idProcesso = processo[0];
+            chegadaProcesso = Number(processo[1]);
+            rajadaProcesso = Number(processo[2]);
+            prioridadeProcesso = Number(processo[3]);
+            inicioProcesso = tempo;
+
+            if (tempo >= chegadaProcesso) {
+                // checa se o processo ja esta no array
+                // caso não esteja, adiciona
+                if (!processosSrtf.includes(idProcesso[1])) {
+                    processosSrtf.push(idProcesso[1]);
+                }
+            }
+        }
+
+        // 2- Obter o tempo de rajada de cada processo da processosSrtf
+        for (let i = 0; i < processosSrtf.length; i++){
+            processo = processos[processosSrtf[i]].split(',');
+
+            processosSrtfRajada.push(processos[processosSrtf[i]][2]);
+        }
+        console.log(tempo, "====", processosSrtfRajada);
+
+
+        // 2- checa se o processo foi completado e atualiza os dados caso tenha sido
+        // 3- popula o vetor dos chegados com processos que nao tenham sido terminados
+        // 4- filtra o vetor temporario e identifica o processo que devera ser executado no instante queueTime
+
+        tempo++;
+    } while (procesosFinalizados.length != processos.length - 1);
+}
+
+/* ======================================================= */
